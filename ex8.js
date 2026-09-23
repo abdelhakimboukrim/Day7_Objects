988011199bc63e1a8cd8dee8db2a07dab43f7795
abdelhakim boukrim <abdelhakim.boukrim3@gmail.com>
2026-09-23 10:40:54 +0000

    Solve: 8. Build an object from variables — Medium

--- ex8.js
+++ ex8.js
@@ -1,6 +1,15 @@
 let name ="omar"
 let age =23
-let person={}
-person.age=age
-person.name=name
-console.log(person)
+let person1={}
+
+//methode 1
+
+person1.name=name
+person1.age=age
+console.log(person1)
+
+
+//methode 2
+let person2={name,age}
+console.log(person2)
+
